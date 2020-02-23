4ea260500dc50538cb9c9841e09b8b2325fce4f3
Programista3 <programista333@gmail.com>
2020-02-23 15:24:55 +0100

    Added PATCH to allowed methods

--- app.js
+++ app.js
@@ -22,8 +22,9 @@ app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
 
 app.use((req, res, next) => {
-    res.header("Access-Control-Allow-Origin", process.env.CLIENT_DOMAIN);
-    res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept, Authorization");
+    res.header('Access-Control-Allow-Origin', process.env.CLIENT_DOMAIN);
+    res.header('Access-Control-Allow-Headers', 'Origin, X-Requested-With, Content-Type, Accept, Authorization');
+    res.header('Access-Control-Allow-Methods', 'GET, POST, OPTIONS, PATCH, PUT, DELETE');
     next();
 });
 
